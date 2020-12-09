b42452200ad06d5d1a1aaf3f9995462227202535
adityabalaji2007 <adityabalaji2007@gmail.com>
2020-12-09 14:19:45 +0530

    modified

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var constraintObject;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -36,6 +37,10 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log = new Log(50,100,150,PI/2);
+
+    constraintObject = new Chain(bird.body,log.body);
+
 }
 
 function draw(){
@@ -61,4 +66,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    log.display();
+    constraintObject.display();
 }
